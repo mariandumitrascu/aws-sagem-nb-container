83a5df854fe9efc435a4da2f262c70bb3cb4ee02
Marian Dumitrascu <md@dumitrascu.net>
2020-01-31 19:24:28 -0600

    first commit

---
+++ run-python3-container.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+docker run -t --name=sagemaker-notebook-container && \
+           -p 8888:8888 && \
+           -e AWS_PROFILE=default-api && \
+           -v ~/.aws:/home/ec2-user/.aws:ro && \                                 # For AWS Credentials
+           -v ~/.ssh:/home/ec2-user/.ssh:ro && \                                 # For Git Credentials
+           -v /var/run/docker.sock:/var/run/docker.sock:ro && \                  # For Docker CLI
+           -v /Users/foobar/projects/SageMaker:/home/ec2-user/SageMaker && \     # For saving work in a host directory
+           # -v .:/home/ec2-user/SageMaker/sagemaker-notebook-container  && \      # For testing
+           dataquadrant/sagemaker-notebook:python3
+
+# Alternatively:
+# docker-compose up sagemaker-notebook-container
